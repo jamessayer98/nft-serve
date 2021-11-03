12588275c62e6030aac95635638ec104e4e70ec7
James Sayer <james.sayer0908@gmail.com>
2021-11-03 21:42:39 +0500

    Initial Implementation of the Gifting Contract is complete

--- contracts/Gift.sol
+++ contracts/Gift.sol
@@ -1,16 +1,221 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4
+pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.7;
 
-contract Gift {
-  address public owner = msg.sender;
+import "./Governable.sol";
+import "./IERC721.sol";
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
+contract Gift is Governable("Contract Deployer") {
+    address public owner = msg.sender;
+
+    // All necessary structures that are required for the functionality of this contract
+    struct NFTGifts {
+        uint256 totalNftsToGift;
+        mapping(address => mapping(uint256 => giftingStatus)) giftedTokensDetails;
+    }
+    struct giftingStatus {
+        bool hasGifted;
+        bool isActive;
+        address giftedTo;
+        bytes32 giftSecret;
+    }
+    mapping(address => NFTGifts) gifts;
+    mapping(address => mapping(uint256 => bool)) allAvailableGifts;
+    mapping(address => mapping(uint256 => address)) allAvailableGifters;
+    // All necessary events that the contract will emit to make sure that the off chain state is
+    // kept in sync with the on chain state
+    event giftAdded(address gifter, address tokenToGift, uint256[] tokenId);
+    event giftRemoved(address gifter, address tokenToGift, uint256 tokenId);
+    event giftAccepted(
+        address gifter,
+        address gifted,
+        address TokenToGift,
+        uint256 tokenId,
+        uint256 acceptedON
     );
-    _;
-  }
 
+    function getMessageHash(
+        address _to,
+        uint256 _amount,
+        string memory _message,
+        uint256 _nonce
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_to, _amount, _message, _nonce));
+    }
 
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        public
+        pure
+        returns (bytes32)
+    {
+        /*
+        Signature is produced by signing a keccak256 hash with the following format:
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        */
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    _messageHash
+                )
+            );
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+
+    function verify(
+        address _signer,
+        address _to,
+        uint256 _amount,
+        string memory _message,
+        uint256 _nonce,
+        bytes memory signature
+    ) public pure returns (bool) {
+        bytes32 messageHash = getMessageHash(_to, _amount, _message, _nonce);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+        return recoverSigner(ethSignedMessageHash, signature) == _signer;
+    }
+
+    // All necessary functions that are required for this contract
+    function addNFTToGift(
+        address tokenAddressToGift,
+        uint256[] memory tokenIdToGift,
+        bytes32[] memory tokenIdGiftSecret
+    ) public payable {
+        // does nothing for now in the next session will add logic to this
+        require(
+            tokenIdGiftSecret.length == tokenIdToGift.length,
+            "Secrets and Token Ids Length has to be equal"
+        );
+        require(tokenIdToGift.length > 0, "Need atleast one token to gift");
+        gifts[msg.sender].totalNftsToGift += tokenIdToGift.length;
+        for (uint256 i = 0; i < tokenIdToGift.length; i++) {
+            gifts[msg.sender].giftedTokensDetails[tokenAddressToGift][
+                    tokenIdToGift[i]
+                ] = giftingStatus({
+                hasGifted: false,
+                giftedTo: (address(0)),
+                giftSecret: tokenIdGiftSecret[i],
+                isActive: true
+            });
+            allAvailableGifts[tokenAddressToGift][tokenIdToGift[i]] = true;
+            allAvailableGifters[tokenAddressToGift][tokenIdToGift[i]] = msg
+                .sender;
+        }
+        emit giftAdded(msg.sender, tokenAddressToGift, tokenIdToGift);
+    }
+
+    function removeNFTFromGifts(uint256 tokenIdToRemove, address tokenAddress)
+        public
+    {
+        require(
+            allAvailableGifts[tokenAddress][tokenIdToRemove] == true,
+            "Token Id is not available in registered gifts"
+        );
+        require(
+            allAvailableGifters[tokenAddress][tokenIdToRemove] == msg.sender,
+            "Only the original gifter can remove the gift from circulation"
+        );
+        require(
+            allAvailableGifters[tokenAddress][tokenIdToRemove] != address(0),
+            "The gift has already been removed"
+        );
+        require(
+            gifts[msg.sender]
+            .giftedTokensDetails[tokenAddress][tokenIdToRemove].hasGifted ==
+                false,
+            "Gift has been delivered which is why we cannot remove it"
+        );
+        allAvailableGifts[tokenAddress][tokenIdToRemove] = false;
+        allAvailableGifters[tokenAddress][tokenIdToRemove] = address(0);
+        gifts[msg.sender].totalNftsToGift--;
+        gifts[msg.sender]
+        .giftedTokensDetails[tokenAddress][tokenIdToRemove].isActive = false;
+        gifts[msg.sender]
+        .giftedTokensDetails[tokenAddress][tokenIdToRemove].hasGifted = true;
+        emit giftRemoved(msg.sender, tokenAddress, tokenIdToRemove);
+    }
+
+    function acceptGift(
+        uint256 tokenIdTo,
+        address tokenAddress,
+        bytes memory secretSignature
+    ) public payable {
+        address originalSigner = recoverSigner(
+            gifts[allAvailableGifters[tokenAddress][tokenIdTo]]
+            .giftedTokensDetails[tokenAddress][tokenIdTo].giftSecret,
+            secretSignature
+        );
+        require(
+            isGoverner(originalSigner) == true,
+            "Gift can only be accepted if signature is provided by an authorized LIT Node"
+        );
+        IERC721 tokenToTransferFrom = IERC721(tokenAddress);
+        tokenToTransferFrom.transferFrom(
+            allAvailableGifters[tokenAddress][tokenIdTo],
+            msg.sender,
+            tokenIdTo
+        );
+        allAvailableGifters[tokenAddress][tokenIdTo] = address(0);
+        allAvailableGifts[tokenAddress][tokenIdTo] = false;
+        gifts[allAvailableGifters[tokenAddress][tokenIdTo]]
+        .giftedTokensDetails[tokenAddress][tokenIdTo].hasGifted = true;
+        gifts[allAvailableGifters[tokenAddress][tokenIdTo]]
+        .giftedTokensDetails[tokenAddress][tokenIdTo].isActive = false;
+        emit giftAccepted(
+            allAvailableGifters[tokenAddress][tokenIdTo],
+            msg.sender,
+            tokenAddress,
+            tokenIdTo,
+            block.timestamp
+        );
+    }
+
+    // This function returns if the gift is available for gifting or not
+    function NFTGiftStatus(uint256 tokenId, address tokenAddress)
+        public
+        view
+        returns (bool)
+    {
+        return allAvailableGifts[tokenAddress][tokenId];
+    }
 }
