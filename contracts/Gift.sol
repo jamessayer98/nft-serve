7b1b55d8ef7adb5bf4accd51d0c69ecbe9e9571d
James Sayer <james.sayer0908@gmail.com>
2021-11-03 21:44:54 +0500

    removed unused functions definations

--- contracts/Gift.sol
+++ contracts/Gift.sol
@@ -33,43 +33,7 @@ contract Gift is Governable("Contract Deployer") {
         uint256 acceptedON
     );
 
-    function getMessageHash(
-        address _to,
-        uint256 _amount,
-        string memory _message,
-        uint256 _nonce
-    ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_to, _amount, _message, _nonce));
-    }
-
-    function getEthSignedMessageHash(bytes32 _messageHash)
-        public
-        pure
-        returns (bytes32)
-    {
-        /*
-        Signature is produced by signing a keccak256 hash with the following format:
-        "\x19Ethereum Signed Message\n" + len(msg) + msg
-        */
-        return
-            keccak256(
-                abi.encodePacked(
-                    "\x19Ethereum Signed Message:\n32",
-                    _messageHash
-                )
-            );
-    }
-
-    function recoverSigner(
-        bytes32 _ethSignedMessageHash,
-        bytes memory _signature
-    ) public pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-    }
-
-    function splitSignature(bytes memory sig)
+   function splitSignature(bytes memory sig)
         public
         pure
         returns (
@@ -100,20 +64,17 @@ contract Gift is Governable("Contract Deployer") {
 
         // implicitly return (r, s, v)
     }
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
 
-    function verify(
-        address _signer,
-        address _to,
-        uint256 _amount,
-        string memory _message,
-        uint256 _nonce,
-        bytes memory signature
-    ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_to, _amount, _message, _nonce);
-        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
-
-        return recoverSigner(ethSignedMessageHash, signature) == _signer;
+        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
+
+  
+
 
     // All necessary functions that are required for this contract
     function addNFTToGift(
